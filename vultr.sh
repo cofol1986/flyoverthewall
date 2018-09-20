1df16e494631c0bb106df16c2e700678fc4c2078
cofol1986 <cofol1986@gmail.com>
2018-09-20 14:05:22 +0800

    Update vultr.sh

--- vultr.sh
+++ vultr.sh
@@ -4,5 +4,8 @@ apt install python-pip -y
 pip install --upgrade setuptools
 pip install wheel
 pip install shadowsocks
+echo "adapt to new openssl api"
+sed -i '/CTX_cleanup/s/CTX_cleanup/CTX_reset/g' /usr/local/lib/python2.7/dist-packages/shadowsocks/crypto/openssl.py
+
 ssserver -p 443 -k ZjEzMzRhZj -m aes-256-cfb --user nobody --workers 5 -d start --fast-open
 
