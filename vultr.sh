d0f60728d07398d5ce7364eeda5db5eefc5a3d28
cofol1986 <cofol1986@gmail.com>
2018-09-20 13:51:54 +0800

    Update vultr.sh

--- vultr.sh
+++ vultr.sh
@@ -4,5 +4,5 @@ apt install python-pip -y
 pip install --upgrade setuptools
 pip install wheel
 pip install shadowsocks
-ssserver -p 443 -k ZjEzMzRhZj -m aes-256-cfb --user nobody --workers 2 -d start --fast-open
+ssserver -p 443 -k ZjEzMzRhZj -m aes-256-cfb --user nobody --workers 5 -d start --fast-open
 
