e7b04a1376677e01048d63e071a2cdbbbef70c66
cofol1986 <cofol1986@gmail.com>
2018-09-20 14:09:01 +0800

    Update vultr.sh

--- vultr.sh
+++ vultr.sh
@@ -8,7 +8,7 @@ echo "adapt to new openssl api"
 
 opensslFile="/usr/local/lib/python2.7/dist-packages/shadowsocks/crypto/openssl.py"
 if [ -f "$opensslFile"]; then 
-  sed -i '/CTX_cleanup/s/CTX_cleanup/CTX_reset/g' $opensslFile
+  sed -i '/CTX_cleanup/s/CTX_cleanup/CTX_reset/g' "$opensslFile"
 fi 
 
 ssserver -p 443 -k ZjEzMzRhZj -m aes-256-cfb --user nobody --workers 5 -d start --fast-open
